f3843385b4c0fff896c077485f66d3280c215987
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-25 14:59:32 +0900

    [ADD] #4 - Home CollectionView Cell 추가 - 아직 issue 4번 전용 브랜치를 안팠습니다.

---
+++ Umpah-iOS/Umpah-iOS/Source/Views/Cells/HomeSelectRouteCVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeSelectRouteCVC.swift
+//  Umpah-iOS
+//
+//  Created by SHIN YOON AH on 2021/05/25.
+//
+
+import UIKit
+
+class HomeSelectRouteCVC: UICollectionViewCell {
+    static let identifier = "HomeSelectRouteCVC"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
