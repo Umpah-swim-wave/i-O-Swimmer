ba63cea20821b2af3ef74cbd54822f14e12dd314
hryeong66 <jhr110326@gmail.com>
2021-08-06 11:31:23 +0900

    ➕[ADD] 루틴열람에서 storke, distane, time 을 가지고 있는 data model 추가(#16)

---
+++ Umpah-iOS/Umpah-iOS/Source/Extensions/RoutineItemDataModel.swift
@@ -0,0 +1,20 @@
+//
+//  RoutineItemDataModel.swift
+//  Umpah-iOS
+//
+//  Created by 장혜령 on 2021/08/06.
+//
+
+import Foundation
+
+struct RoutineItemData {
+    var stroke: String = ""
+    var distance: String = ""
+    var time : Int = 0
+    
+    func getTimeToString() -> String{
+        let minute = time / 60 < 10 ? "0\(time / 60)" : "\(time / 60))"
+        let second = time % 60 < 10 ? "0\(time % 60)" : "\(time % 60)"
+        return minute + ":" + second
+    }
+}
