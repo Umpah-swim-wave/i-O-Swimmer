9877fc90b5eb87b0360acbd667f26bfd3c5ff30f
hryeong66 <jhr110326@gmail.com>
2021-08-08 10:16:10 +0900

    🔀[MERGE] merge develop (#16)

---
+++ Umpah-iOS/Cells/RoutineScene/RoutineItemTVC.swift
@@ -0,0 +1,82 @@
+//
+//  RoutineItemTVC.swift
+//  Umpah-iOS
+//
+//  Created by 장혜령 on 2021/08/05.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class RoutineItemTVC: UITableViewCell {
+    static let identifier = "RoutineItemTVC"
+    
+    private var routineItem: RoutineItemData?
+    
+    private var strokeLabel = UILabel().then{
+        $0.font = .systemFont(ofSize: 14)
+    }
+    private var distanceLabel = UILabel().then{
+        $0.font = .systemFont(ofSize: 14)
+    }
+    private var timeLabel = UILabel().then{
+        $0.font = .systemFont(ofSize: 14)
+    }
+    
+    private var lineView = UIView().then {
+        $0.backgroundColor = .lightGray
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupLayout()
+        setContentText()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    private func setupLayout(){
+        addSubviews([strokeLabel,
+                     distanceLabel,
+                     timeLabel,
+                     lineView])
+        
+        strokeLabel.snp.makeConstraints{
+            $0.top.bottom.equalToSuperview().inset(15)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        
+        distanceLabel.snp.makeConstraints{
+            $0.centerY.equalTo(strokeLabel.snp.centerY)
+            $0.leading.equalToSuperview().inset(191)
+        }
+        
+        timeLabel.snp.makeConstraints {
+            $0.centerY.equalTo(strokeLabel.snp.centerY)
+            $0.trailing.equalToSuperview().inset(32)
+        }
+        
+        lineView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+    }
+
+    public func setRutineItem(item: RoutineItemData){
+        routineItem = item
+    }
+    
+    private func setContentText(){
+        guard let item = routineItem else {
+            print("넘겨져온 item이 없음")
+            return
+        }
+        strokeLabel.text = item.stroke
+        distanceLabel.text = item.distance
+        timeLabel.text = item.getTimeToString()
+    }
+}
---
+++ Umpah-iOS/Components/Shared/XmarkButton.swift
@@ -0,0 +1,37 @@
+//
+//  XmarkButton.swift
+//  Umpah-iOS
+//
+//  Created by 장혜령 on 2021/08/05.
+//
+
+import UIKit
+
+class XmarkButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(toDismiss vc: UIViewController) {
+        super.init(frame: .zero)
+        setupConfigure()
+        initAction(vc: vc)
+    }
+    
+    private func setupConfigure() {
+        setImage(UIImage(named: "exitButton"), for: .normal)
+    }
+    
+    private func initAction(vc: UIViewController) {
+        let dismissAction = UIAction { _ in
+            vc.dismiss(animated: true, completion: nil)
+        }
+        addAction(dismissAction, for: .touchUpInside)
+    }
+
+}
