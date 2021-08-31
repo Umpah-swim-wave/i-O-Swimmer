0aa798203cd3fab25fd2be51f3ecf51045ef16d3
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-08-31 21:15:06 +0900

    ➕[ADD] Day Cell 구현(#29)

--- Umpah-iOS/Umpah-iOS/Source/Views/Cells/Main/ExpandedDayTVC.swift
+++ Umpah-iOS/Umpah-iOS/Source/Views/Cells/Main/ExpandedDayTVC.swift
@@ -19,6 +19,22 @@ class ExpandedDayTVC: UITableViewCell {
         $0.font = .boldSystemFont(ofSize: 12)
         $0.textColor = .orange
     }
+    let strokeLabel = UILabel().then {
+        $0.text = "자유형"
+        $0.font = .systemFont(ofSize: 14)
+    }
+    let distanceLabel = UILabel().then {
+        $0.text = "999m"
+        $0.font = .systemFont(ofSize: 14)
+    }
+    let velocityLabel = UILabel().then {
+        $0.text = "1.7m/s"
+        $0.font = .systemFont(ofSize: 14)
+    }
+    let timeLabel = UILabel().then {
+        $0.text = "99:99"
+        $0.font = .systemFont(ofSize: 14)
+    }
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -30,11 +46,35 @@ class ExpandedDayTVC: UITableViewCell {
     }
     
     fileprivate func setupLayout() {
-        addSubview(rowLabel)
+        addSubviews([rowLabel,
+                     strokeLabel,
+                     distanceLabel,
+                     velocityLabel,
+                     timeLabel])
         
         rowLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(16)
             $0.top.bottom.equalToSuperview().inset(20)
+        }
+        
+        strokeLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(46)
+            $0.centerY.equalToSuperview()
+        }
+        
+        distanceLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(146)
+            $0.centerY.equalToSuperview()
+        }
+        
+        velocityLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(81)
+            $0.centerY.equalToSuperview()
+        }
+        
+        timeLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(24)
+            $0.centerY.equalToSuperview()
         }
     }
 
