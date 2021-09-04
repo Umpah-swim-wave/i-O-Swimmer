0d7853f93ebfa3d5628ab6d2bdb77c3faeb5c989
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-09-05 06:51:50 +0900

    ➕[ADD] Date Lable 생성(#28)

--- Umpah-iOS/Umpah-iOS/Source/Views/Cells/Main/DetailTVC.swift
+++ Umpah-iOS/Umpah-iOS/Source/Views/Cells/Main/DetailTVC.swift
@@ -15,11 +15,22 @@ class DetailTVC: UITableViewCell {
     static let identifier = "DetailTVC"
     
     // MARK: - Properties
+    let dateLabel = UILabel().then {
+        $0.text = "2021.09.05"
+        $0.font = .boldSystemFont(ofSize: 16)
+        $0.addCharacterSpacing(kernValue: 2)
+    }
     let titleLabel = UILabel().then {
-        $0.text = "THIS WEEK"
+        $0.text = "OVERVIEW"
         $0.textColor = .gray
         $0.font = .boldSystemFont(ofSize: 12)
         $0.addCharacterSpacing(kernValue: 2)
+    }
+    let titleStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .fill
+        $0.distribution = .fillEqually
+        $0.spacing = 24
     }
     let detailView = DetailView()
 
@@ -36,15 +47,17 @@ class DetailTVC: UITableViewCell {
     private func setupLayout() {
         sendSubviewToBack(contentView)
         
-        addSubviews([titleLabel, detailView])
+        addSubviews([titleStack, detailView])
+        titleStack.addArrangedSubview(dateLabel)
+        titleStack.addArrangedSubview(titleLabel)
         
-        titleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(40)
+        titleStack.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(37)
             $0.leading.equalToSuperview().inset(32)
         }
         
         detailView.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.top.equalTo(titleStack.snp.bottom).offset(16)
             $0.leading.trailing.equalToSuperview()
             $0.bottom.equalToSuperview().inset(49)
         }
