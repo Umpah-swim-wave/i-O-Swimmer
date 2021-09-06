6a01a808fdee8b677f17127d983cd7247955d9ad
hryeong66 <jhr110326@gmail.com>
2021-09-06 13:35:33 +0900

    ✅[CHORE] black color 추가

--- Umpah-iOS/Umpah-iOS/Source/Extensions/UIColor+.swift
+++ Umpah-iOS/Umpah-iOS/Source/Extensions/UIColor+.swift
@@ -5,4 +5,10 @@
 //  Created by 장혜령 on 2021/09/06.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    class func upuhBlack() -> UIColor {
+        return UIColor(red: 0.224, green: 0.224, blue: 0.224, alpha: 1)
+    }
+}
