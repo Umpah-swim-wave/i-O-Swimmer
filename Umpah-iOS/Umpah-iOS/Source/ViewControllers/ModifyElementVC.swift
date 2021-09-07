9e80e879e5602f651206641d33d1f98055657e86
hryeong66 <jhr110326@gmail.com>
2021-09-07 20:07:15 +0900

    ➕[ADD] 요소 선택하여 수정하는 뷰 관련 파일 추가 (#27)

---
+++ Umpah-iOS/Umpah-iOS/Source/ViewControllers/ModifyElementVC.swift
@@ -0,0 +1,25 @@
+//
+//  ModifySelectionVC.swift
+//  Umpah-iOS
+//
+//  Created by 장혜령 on 2021/09/07.
+//
+
+import UIKit
+
+class ModifySelectionVC: UIViewController {
+
+    static let identifier = "ModifySelectionVC"
+    
+    private var backgroundImageView = UIImageView()
+    public var backgroundImage : UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
