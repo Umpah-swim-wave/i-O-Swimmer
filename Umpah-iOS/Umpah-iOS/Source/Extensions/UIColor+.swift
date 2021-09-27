b56ec0a569dc81cd5efa8bbe3aefe3edd1a8d5ad
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-09-27 17:26:35 +0900

    ➕[ADD] UIColor 및 Font 재추가(#32)

--- Umpah-iOS/Umpah-iOS/Source/Extensions/UIColor+.swift
+++ Umpah-iOS/Umpah-iOS/Source/Extensions/UIColor+.swift
@@ -5,4 +5,46 @@
 //  Created by SHIN YOON AH on 2021/09/27.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    @nonobjc class var upuhBlack: UIColor {
+        return UIColor(red: 57.0 / 255.0, green: 57.0 / 255.0, blue: 57.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var upuhOrange: UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 154.0 / 255.0, blue: 97.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var upuhGreen: UIColor {
+        return UIColor(red: 129.0 / 255.0, green: 151.0 / 255.0, blue: 154.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var upuhBlue: UIColor {
+        return UIColor(red: 78.0 / 255.0, green: 149.0 / 255.0, blue: 185.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var upuhSubOrange: UIColor {
+        return UIColor(red: 1.0, green: 195.0 / 255.0, blue: 161.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var upuhGray: UIColor {
+        return UIColor(red: 207.0 / 255.0, green: 214.0 / 255.0, blue: 215.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var upuhBeginner: UIColor {
+        return UIColor(red: 221.0 / 255.0, green: 248.0 / 255.0, blue: 187.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var upuhBadgeOrange: UIColor {
+        return UIColor(red: 1.0, green: 168.0 / 255.0, blue: 140.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var upuhIntermediate: UIColor {
+        return UIColor(red: 233.0 / 255.0, green: 221.0 / 255.0, blue: 253.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var upuhMaster: UIColor {
+        return UIColor(red: 1.0, green: 210.0 / 255.0, blue: 210.0 / 255.0, alpha: 1.0)
+    }
+}
