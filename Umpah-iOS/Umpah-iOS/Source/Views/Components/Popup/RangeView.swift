6a68d6d0335c540e77eb625187255b07ebadfeca
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-09-27 22:02:38 +0900

    ➕[ADD] Popup View 구성 완료(#34)

--- Umpah-iOS/Umpah-iOS/Source/Views/Components/Popup/RangeView.swift
+++ Umpah-iOS/Umpah-iOS/Source/Views/Components/Popup/RangeView.swift
@@ -17,10 +17,16 @@ class RangeView: UIView {
         $0.text = "기본값은 가장 최근 기록입니다!"
         $0.font = .systemFont(ofSize: 11)
     }
+    let buttonStackView = UIStackView().then {
+        $0.alignment = .center
+        $0.axis = .horizontal
+        $0.distribution = .fillEqually
+        $0.spacing = 12
+    }
     let dayButton = UIButton().then {
         $0.setTitle("일간", for: .normal)
         $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
-        $0.tintColor = .systemTeal
+        $0.setTitleColor(.systemTeal, for: .normal)
         $0.layer.borderColor = UIColor.systemTeal.withAlphaComponent(0.3).cgColor
         $0.layer.borderWidth = 2
         $0.layer.cornerRadius = 24
@@ -29,7 +35,7 @@ class RangeView: UIView {
     let weekButton = UIButton().then {
         $0.setTitle("주간", for: .normal)
         $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
-        $0.tintColor = .systemTeal
+        $0.setTitleColor(.systemTeal, for: .normal)
         $0.layer.borderColor = UIColor.systemTeal.withAlphaComponent(0.3).cgColor
         $0.layer.borderWidth = 2
         $0.layer.cornerRadius = 24
@@ -38,7 +44,7 @@ class RangeView: UIView {
     let monthButton = UIButton().then {
         $0.setTitle("월간", for: .normal)
         $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
-        $0.tintColor = .systemTeal
+        $0.setTitleColor(.systemTeal, for: .normal)
         $0.layer.borderColor = UIColor.systemTeal.withAlphaComponent(0.3).cgColor
         $0.layer.borderWidth = 2
         $0.layer.cornerRadius = 24
@@ -55,13 +61,24 @@ class RangeView: UIView {
     }
     
     private func setupLayout() {
-        addSubviews([titleLabel, dayButton, weekButton, monthButton, infoLabel])
+        addSubviews([titleLabel, buttonStackView, infoLabel])
+        buttonStackView.addArrangedSubview(dayButton)
+        buttonStackView.addArrangedSubview(weekButton)
+        buttonStackView.addArrangedSubview(monthButton)
         
         titleLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().inset(32)
         }
         
+        buttonStackView.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(32)
+        }
         
+        infoLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(buttonStackView.snp.bottom).offset(20)
+        }
     }
 }
