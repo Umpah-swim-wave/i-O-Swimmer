49dafca8316eb0ac44a585270fa6d0f6ae6153ad
hryeong66 <jhr110326@gmail.com>
2021-09-29 02:09:31 +0900

    ✅[CHORE] routine storage와 data binding (#27)

--- Umpah-iOS/Umpah-iOS/Source/ViewControllers/RoutineViewController.swift
+++ Umpah-iOS/Umpah-iOS/Source/ViewControllers/RoutineViewController.swift
@@ -17,18 +17,15 @@ class RoutineViewController: UIViewController {
     private var viewModel = RoutineViewModel()
     private var disposeBag = DisposeBag()
     
-    //MARK: TableView data
-    private var routineList: [String : [RoutineItemData]] = [:]
-    private var tableCellList : [RoutineSetTVC] = []
-    private var tableSetTitleList : [String] = ["warm-up", "main", "cool-down"]
+    let upuhLightGreen =  UIColor(red: 246.0 / 255.0, green: 250.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
     
-    private let upuhGreen = UIColor(red: 0.965, green: 0.98, blue: 0.988, alpha: 1)
+    //MARK: TableView data
+    private var tableCellList : [RoutineSetTVC] = []
+    
     //MARK: - UI Component
     private let navigationView = UIView()
-    private var tableView = UITableView().then{
-        $0.registerCustomXib(name: RoutineSetTVC.identifier)
-        $0.separatorStyle = .none
-    }
+    private var tableView = UITableView()
+    private let tableViewHeader = UIView()
     
     private lazy var bottomButton = UIButton().then {
         $0.layer.cornerRadius = 16
@@ -38,13 +35,12 @@ class RoutineViewController: UIViewController {
         $0.addTarget(self, action: #selector(changeTableViewEditingMode), for: .touchUpInside)
     }
     
-    private let tableViewHeader = UIView()
-    
-    private let titleTextField = UITextField().then{
+    private lazy var titleTextField = UITextField().then{
         $0.font = .boldSystemFont(ofSize: 20)
         $0.isUserInteractionEnabled = false
         $0.text = "1km 기본루틴"
         $0.returnKeyType = .done
+        $0.delegate = self
     }
     private let titleUnderlineView = UIView()
     
@@ -80,8 +76,8 @@ class RoutineViewController: UIViewController {
     private let descriptionTextView = UITextView().then{
         $0.text = "설명이 들어갑니다 오랜만에 수영을 하면 몸이 굳으니까 이걸로 몸을 푸는거라고 합니다 그렇다네요 최대두줄까지 들어가나요?"
         $0.font = .systemFont(ofSize: 13)
-        $0.textColor = .black
-        $0.backgroundColor = UIColor(red: 0.965, green: 0.98, blue: 0.988, alpha: 1)
+        $0.textColor = .upuhBlack
+        $0.backgroundColor = .clear
         $0.textContainer.maximumNumberOfLines = 2
         $0.textContainer.lineBreakMode = .byTruncatingTail
         $0.isUserInteractionEnabled = false
@@ -95,10 +91,154 @@ class RoutineViewController: UIViewController {
         super.viewDidLoad()
         setupConstraints()
         setTableViewAttribute()
-        //initRoutineSetCells()
         bindViewModel()
-        view.backgroundColor = upuhGreen
+        view.backgroundColor = upuhLightGreen
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(view.endEditing))
+        view.addGestureRecognizer(tapGesture)
     }
+    
+
+    @objc
+    func changeTableViewEditingMode(){
+        let mode = tableView.isEditing ? false : true
+        tableView.isEditing = mode
+        tableView.tableFooterView = mode ? getFooterViewLayout() : nil
+        updateHeaderLayout(atEditMode: mode)
+        tableCellList.forEach{
+            $0.tableView.setEditing(mode, animated: true)
+            $0.isEditingMode = mode
+        }
+        updateTableView()
+        if mode{
+            titleTextField.becomeFirstResponder()
+            descriptionTextView.isUserInteractionEnabled = true
+        }else{
+            descriptionTextView.isUserInteractionEnabled = false
+        }
+        
+    }
+    
+    private func getRoutineSetCell(setTitle: String) -> RoutineSetTVC {
+        guard let cell = self.tableView.dequeueReusableCell(withIdentifier: RoutineSetTVC.identifier) as? RoutineSetTVC else { return RoutineSetTVC()}
+        cell.setRoutineContent(title: setTitle, viewModel: viewModel)
+        cell.isEditingMode = tableView.isEditing
+        return cell
+    }
+    
+    private func bindViewModel(){
+        
+        viewModel.routineStorage.getRoutineList()
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { routineList in
+                print("지금 불려서 다시 생성됨!")
+                self.tableView.beginUpdates()
+                var cellList : [RoutineSetTVC] = []
+                self.viewModel.routineStorage.routineSetTitleList.forEach{
+                    let cell = self.getRoutineSetCell(setTitle: $0)
+                    cellList.append(cell)
+                }
+                self.tableCellList = cellList
+
+                //self.tableView.reloadData()
+                self.tableView.endUpdates()
+            }).disposed(by: disposeBag)
+        
+        viewModel.routineStorage.getRoutineList()
+            .bind(to: tableView.rx.items(cellIdentifier: RoutineSetTVC.identifier,
+                                         cellType: RoutineSetTVC.self)){ (index, element, cell) in
+                print("onNext 이벤트로 다시 불림!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                let routineTitle = self.viewModel.routineStorage.routineSetTitleList[index]
+                
+                cell.setRoutineContent(title: routineTitle, viewModel: self.viewModel)
+                cell.isEditingMode = self.tableView.isEditing
+                if self.tableCellList.count > index{
+                    self.tableCellList.remove(at: index)
+                    self.tableCellList.insert(cell, at: index)
+                }else{
+                    self.tableCellList.insert(cell, at: index)
+                }
+            }.disposed(by: disposeBag)
+    }
+    
+    private func setTableViewAttribute(){
+        tableView.delegate = nil
+        //tableView.dataSource = self
+        tableView.registerCustomXib(name: RoutineSetTVC.identifier)
+        tableView.separatorStyle = .none
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
+        tableView.backgroundColor = .clear
+    }
+    
+    func updateTableView(){
+        tableView.beginUpdates()
+        tableView.reloadData()
+        tableView.endUpdates()
+    }
+}
+
+extension RoutineViewController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let setTitle = viewModel.routineStorage.routineSetTitleList[indexPath.row]
+        let routineCount = viewModel.routineStorage.routineList[setTitle]?.count ?? 0
+        print("setTitle = \(setTitle), routineCount = \(routineCount)")
+        return tableView.isEditing ? CGFloat(170 + routineCount * 44) : CGFloat(120 + routineCount * 44)
+    }
+    
+    internal func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        tableCellList.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+}
+
+extension RoutineViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.routineStorage.routineSetTitleList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableCellList[indexPath.row]
+    }
+
+}
+
+
+extension RoutineViewController: RoutineCellDelegate{
+    func routineItemCellForAdding(cell: RoutineSetTVC) {
+        print("이거 왜 안눌려??")
+        let index = cell.getTableCellIndexPathRow()
+        updateTableView()
+    }
+    
+    func routineItemCellForDeleting(cell: RoutineSetTVC, index: Int) {
+        let index = cell.getTableCellIndexPathRow()
+        updateTableView()
+    }
+    
+   
+}
+
+
+extension RoutineViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension RoutineViewController {
     
     private func updateHeaderLayout(atEditMode: Bool){
         let offsetValue = atEditMode ? 8 : 0
@@ -112,99 +252,38 @@ class RoutineViewController: UIViewController {
         titleTextField.isUserInteractionEnabled = atEditMode ? true : false
         titleTextField.text = atEditMode ? "Copy of " + titleTextField.text! : titleTextField.text
     }
+    
+    func getFooterViewLayout() -> UIView{
+        let view = UIView(frame: CGRect(x: 0, y: 0,
+                                        width: UIScreen.getDeviceWidth(),
+                                        height: 68))
 
-    private func initRoutineSetCells(){
-        tableSetTitleList.forEach{
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: RoutineSetTVC.identifier) as? RoutineSetTVC else { return }
-            cell.setRoutineContent(title: $0, itemList: routineList[$0] ?? [])
-            cell.cellDelegate = self
-            cell.viewModel = viewModel
-            tableCellList.append(cell)
+        let titleButton = UIButton().then{
+            $0.setTitle("세트 추가하기", for: .normal)
+            $0.titleLabel?.font = .systemFont(ofSize: 14)
+            $0.setTitleColor(.gray, for: .normal)
         }
-    }
-    
-    @objc
-    func changeTableViewEditingMode(){
-        let mode = tableView.isEditing ? false : true
-        tableView.isEditing = mode
-        tableView.tableFooterView = mode ? getFooterViewLayout() : nil
-        updateHeaderLayout(atEditMode: mode)
-        tableCellList.forEach{
-            $0.tableView.setEditing(mode, animated: true)
-            $0.isEditingMode = mode
+        
+        let titleUnderline = UIView().then{
+            $0.backgroundColor = .gray
         }
-        tableView.reloadData()
-        tableCellList.forEach{
-            if $0.showsReorderControl {
-                //$0.reorderControlImageView?.frame = CGRect(x: 0, y: 0, width: 25, height: 13)
-                print("---------------------------------")
-                print("\($0.reorderControlImageView)")
-                print("---------------------------------")
-            }
+        
+        view.addSubviews([titleButton,
+                          titleUnderline])
+        titleButton.snp.makeConstraints{
+            $0.center.equalToSuperview()
         }
+        
+        titleUnderline.snp.makeConstraints{
+            $0.top.equalTo(titleButton.snp.bottom).offset(-3)
+            $0.centerX.equalTo(titleButton.snp.centerX)
+            $0.width.equalTo(titleButton.snp.width)
+            $0.height.equalTo(1)
+        }
+        return view
     }
-    
-    private func bindViewModel(){
-        tableView.rx.setDelegate(self).disposed(by: disposeBag)
 
-        viewModel.routineSubject?.bind(to: tableView.rx.items(cellIdentifier: RoutineSetTVC.identifier, cellType: RoutineSetTVC.self)){ row, element, cell in
-            cell.setRoutineContent(title: self.viewModel.routineSetTitleList[row],
-                                   viewModel: self.viewModel)
-        }.disposed(by: disposeBag)
-    }
     
-    private func setTableViewAttribute(){
-        tableView.delegate = nil
-        tableView.dataSource = nil
-        tableView.backgroundColor = upuhGreen
-    }
-}
-
-extension RoutineViewController: UITableViewDelegate{
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let setTitle = tableSetTitleList[indexPath.row]
-        let routineCount = routineList[setTitle]?.count ?? 0
-        return tableView.isEditing ? CGFloat(170 + routineCount * 44) : CGFloat(120 + routineCount * 44)
-    }
-    
-    private func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        tableCellList.swapAt(sourceIndexPath.row, destinationIndexPath.row)
-    }
-    
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .none
-    }
-}
-
-
-extension RoutineViewController: RoutineCellDelegate{
-    func routineItemCellForAdding(cell: RoutineSetTVC) {
-        print("이거 왜 안눌려??")
-        let index = cell.getTableCellIndexPathRow()
-//        routineList[cell.setTitle]?.append(RoutineItemData())
-//        tableCellList[index].routineItemList = routineList[cell.setTitle] ?? []
-        updateTableView()
-    }
-    
-    func routineItemCellForDeleting(cell: RoutineSetTVC, index: Int) {
-        let index = cell.getTableCellIndexPathRow()
-       // routineList[cell.setTitle]?.remove(at: index)
-        updateTableView()
-    }
-    
-    func updateTableView(){
-        tableView.beginUpdates()
-        tableView.reloadData()
-        tableView.endUpdates()
-    }
-}
-
-extension RoutineViewController {
     private func setupConstraints(){
         view.addSubviews([navigationView,
                           tableView,
@@ -246,11 +325,12 @@ extension RoutineViewController {
         titleTextField.snp.makeConstraints{
             $0.top.equalToSuperview().offset(24)
             $0.leading.equalToSuperview().offset(28)
+            $0.trailing.equalToSuperview().inset(41)
         }
         
         titleUnderlineView.snp.makeConstraints{
             $0.top.equalTo(titleTextField.snp.bottom)
-            $0.leading.equalTo(titleTextField.snp.leading)
+            $0.leading.equalToSuperview().offset(24)
             $0.trailing.equalToSuperview().inset(41)
             $0.height.equalTo(1)
         }
@@ -288,33 +368,4 @@ extension RoutineViewController {
         tableView.tableHeaderView = tableViewHeader
     }
     
-    func getFooterViewLayout() -> UIView{
-        let view = UIView(frame: CGRect(x: 0, y: 0,
-                                        width: UIScreen.getDeviceWidth(),
-                                        height: 68))
-
-        let titleButton = UIButton().then{
-            $0.setTitle("세트 추가하기", for: .normal)
-            $0.titleLabel?.font = .systemFont(ofSize: 14)
-            $0.setTitleColor(.gray, for: .normal)
-        }
-        
-        let titleUnderline = UIView().then{
-            $0.backgroundColor = .gray
-        }
-        
-        view.addSubviews([titleButton,
-                          titleUnderline])
-        titleButton.snp.makeConstraints{
-            $0.center.equalToSuperview()
-        }
-        
-        titleUnderline.snp.makeConstraints{
-            $0.top.equalTo(titleButton.snp.bottom).offset(-3)
-            $0.centerX.equalTo(titleButton.snp.centerX)
-            $0.width.equalTo(titleButton.snp.width)
-            $0.height.equalTo(1)
-        }
-        return view
-    }
 }
