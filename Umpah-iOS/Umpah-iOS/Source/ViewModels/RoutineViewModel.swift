1b27e00012f9818c7c3c9faeb356aec9d82cdc05
hryeong66 <jhr110326@gmail.com>
2021-10-11 05:27:06 +0900

    ✨[FEAT] 전체 거리, 시간을 계산하기 위한 함수 분리하며 구현(#27)

--- Umpah-iOS/Umpah-iOS/Source/ViewModels/RoutineViewModel.swift
+++ Umpah-iOS/Umpah-iOS/Source/ViewModels/RoutineViewModel.swift
@@ -12,10 +12,60 @@ class RoutineViewModel {
     
     var routineStorage = RoutineStorage()
     
-    
     func getRoutineItemCount(index: Int) -> Int{
         let setTitle = routineStorage.routineSetTitleList[index]
         return routineStorage.routineList[setTitle]?.count ?? 0
     }
     
+    func getTotalRoutinesDistanceToString() -> String{
+        let sum = getTotalDistanceInAllRoutines()
+        return String(format: "%.1f", Float(sum) / 1000.0) + "km"
+    }
+    
+    func getTotalRoutinesTimeToString() -> String{
+        return getTimeToString(time: getTotalTimeInAllRoutines())
+    }
+    
+    func getTotalDistanceInAllRoutines() -> Int{
+        var sum = 0
+        routineStorage.routineSetTitleList.forEach{
+            sum += self.getTotalDistanceInSetRoutines(setTitle: $0)
+        }
+        return sum
+    }
+    
+    private func getTotalDistanceInSetRoutines(setTitle: String) -> Int{
+        var sum = 0
+        routineStorage.routineList[setTitle]?.forEach {
+            sum += $0.distance
+        }
+        return sum
+    }
+
+        
+    func getTotalTimeInAllRoutines() -> Int{
+        var sum = 0
+        routineStorage.routineSetTitleList.forEach{
+            sum += self.getTotalTimeInSetRoutines(setTitle: $0)
+        }
+        return sum
+    }
+    
+    private func getTotalTimeInSetRoutines(setTitle: String) -> Int{
+        var sum = 0
+        routineStorage.routineList[setTitle]?.forEach {
+            sum += $0.time
+        }
+        return sum
+    }
+    
+    func getTotalTimeToString() -> String{
+        return getTimeToString(time: getTotalTimeInAllRoutines())
+    }
+    
+    func getTimeToString(time: Int) -> String{
+        let hour = time / 3600 < 10 ? "0\(time / 3600)" : "\(time / 3600)"
+        let minute = (time % 3600)/60 < 10 ? "0\((time % 3600)/60)" : "\((time % 3600)/60)"
+        return hour + "h " + minute + "m"
+    }
 }
