760eefcebe564f7c547750552f664680e780bee4
hryeong66 <jhr110326@gmail.com>
2021-10-12 16:02:59 +0900

    ➕[ADD] main에서 사용할 routine cell 추가 (#47)

---
+++ Umpah-iOS/Umpah-iOS/Source/Views/Cells/Main/RoutineTVC.swift
@@ -0,0 +1,23 @@
+//
+//  RoutineTVC.swift
+//  Umpah-iOS
+//
+//  Created by 장혜령 on 2021/10/12.
+//
+
+import UIKit
+
+class RoutineTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
