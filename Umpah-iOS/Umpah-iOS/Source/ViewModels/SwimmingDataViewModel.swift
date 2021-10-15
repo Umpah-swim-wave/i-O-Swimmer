e68aee05c89bd4cde772e36443a4b7aa4963e80a
hryeong66 <jhr110326@gmail.com>
2021-10-16 03:16:03 +0900

    ✨[FEAT] 처음에 swimming data를 뽑아올 수 있도록 함수 추가(#30)

--- Umpah-iOS/Umpah-iOS/Source/ViewModels/SwimmingDataViewModel.swift
+++ Umpah-iOS/Umpah-iOS/Source/ViewModels/SwimmingDataViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class SwimmingDataViewModel{
+    let swimmingStorage = SwimmingDataStorage()
+    var swimmingWorkoutList: [SwimWorkoutData] = []
+    
+    func initSwimmingData(){
+        swimmingStorage.loadWorkoutHKSource()
+        swimmingStorage.refineSwimmingWorkoutData(completion: { workoutList, error in
+            self.swimmingWorkoutList = workoutList
+            self.getStrokeAndDistanceData()
+        })
+    }
+    
+    private func getStrokeAndDistanceData(){
+        for index in 0..<swimmingWorkoutList.count {
+            swimmingStorage.refineSwimmingStrokeData(start: swimmingWorkoutList[index].startDate,
+                                                     end:  swimmingWorkoutList[index].endDate) { strokes, error in
+                self.swimmingWorkoutList[index].strokeList = strokes
+            }
+            swimmingStorage.refineSwimmingDistanceData(start: swimmingWorkoutList[index].startDate,
+                                                       end: swimmingWorkoutList[index].endDate) { distances, error in
+                self.swimmingWorkoutList[index].distanceList = distances
+            }
+        }
+    }
+    
+}
