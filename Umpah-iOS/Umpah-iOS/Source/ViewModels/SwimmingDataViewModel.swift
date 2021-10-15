4763a07d51f429b1c6458b2ac7b69200fd3af3fb
hryeong66 <jhr110326@gmail.com>
2021-10-16 03:41:57 +0900

    ✨[FEAT] workout list 데이터들 csv 파일로 추출할 수 있도록 추가 (#30)

--- Umpah-iOS/Umpah-iOS/Source/ViewModels/SwimmingDataViewModel.swift
+++ Umpah-iOS/Umpah-iOS/Source/ViewModels/SwimmingDataViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CSV
 
 class SwimmingDataViewModel{
     let swimmingStorage = SwimmingDataStorage()
@@ -30,6 +31,49 @@ class SwimmingDataViewModel{
                 self.swimmingWorkoutList[index].distanceList = distances
             }
         }
+    }
+    
+    private func makeSwimmingDateToCSVFile(){
+        let documentURL = setNewCSVFile(fileName: "swimmingDetailData.csv")
+        print("-----------------------")
+        print(documentURL)
+        print("-----------------------")
+        
+        let stream = OutputStream(url: documentURL , append: true)!
+        let csv = try! CSVWriter(stream: stream)
+        try! csv.write(row: ["시작시간", "종료시간", "운동시간", "style번호"])
+        
+        for workout in swimmingWorkoutList {
+            for index in 0..<workout.distanceList.count{
+                var rowData: [String] = []
+                rowData.append(workout.distanceList[index].startDate.toKoreaTime())
+                rowData.append(workout.distanceList[index].endDate.toKoreaTime())
+                rowData.append(workout.distanceList[index].timeInterval.description)
+                rowData.append(workout.strokeList[index].strokeStyle.description)
+                try! csv.write(row: rowData)
+            }
+            try! csv.write(row: ["------","------","------","------"])
+        }
+        
+    }
+    
+    func setNewCSVFile(fileName: String) -> URL{
+        //fileManager 이용
+        //core data 도 함께
+        print(NSHomeDirectory())
+        let fileManager = FileManager.default // 파일 매니저의 싱글톤 객체 리턴
+        
+        // document dir url
+        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        print(documentsURL)
+        
+        let fileURL = documentsURL.appendingPathComponent(fileName)
+        let myText = NSString(string:"")
+
+        //빈파일로 다시 만들기
+        try? myText.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8.rawValue)
+        return fileURL
+        
     }
     
 }
