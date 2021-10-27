ccd419e7e745d0d6d4c1539299a893de690731ea
hryeong66 <jhr110326@gmail.com>
2021-10-28 01:03:24 +0900

    ✅[CHORE] model 이름 변경 및 display 함수 추가 (#55)

--- Umpah-iOS/Umpah-iOS/Source/Models/Record/SwimmingRecordRequest.swift
+++ Umpah-iOS/Umpah-iOS/Source/Models/Record/SwimmingRecordRequest.swift
@@ -8,27 +8,47 @@
 import Foundation
 
 // MARK: - Welcome
-struct SwimRecordRequest: Codable {
+struct SwimmingRecordRequest: Codable {
     let userID: Int
-    let workoutList: [WorkoutList]
+    let workoutList: [SwimmingWorkoutData]
 
     enum CodingKeys: String, CodingKey {
         case userID = "userId"
         case workoutList
     }
+    
+    
 }
 
 // MARK: - WorkoutList
-struct WorkoutList: Codable {
+struct SwimmingWorkoutData: Codable {
     let startWorkoutDate: String
     let totalBeatPerMinute, totalEnergyBurned: Double
     let distancePerLabs, totalSwimmingStrokeCount: Int
-    let recordLabsList: [RecordLabsList]
+    let recordLabsList: [RecordLab]
+    
+    func display(){
+        print("---------------------------")
+        print("startDate =  \(startWorkoutDate)")
+        print("totalBeatPerMinute =  \(totalBeatPerMinute)")
+        print("totalEnergyBurned =  \(totalEnergyBurned)")
+        print("distancePerLabs =  \(distancePerLabs)")
+        print("totalSwimmingStrokeCount =  \(totalSwimmingStrokeCount)")
+        print("recordLabsList =  \(recordLabsList)")
+        print("---------------------------")
+    }
 }
 
 // MARK: - RecordLabsList
-struct RecordLabsList: Codable {
+struct RecordLab: Codable {
     let date: String
     let time: Double
     let strokeType: Int
+}
+
+struct CommonRespose: Codable {
+    let status: Int
+    let message: String
+    let success: Bool
+    let data: String?
 }
