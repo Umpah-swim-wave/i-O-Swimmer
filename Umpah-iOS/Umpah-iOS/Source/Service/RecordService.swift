045c46865c11bc7bd40f3d47fe9eeb51d1710b37
hryeong66 <jhr110326@gmail.com>
2021-10-28 01:25:09 +0900

    ✨[FEAT] record Service 서버 연결 구현 (#55)

--- Umpah-iOS/Umpah-iOS/Source/Service/RecordService.swift
+++ Umpah-iOS/Umpah-iOS/Source/Service/RecordService.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import Moya
+
+enum RecordService{
+    case sendSwimmingRecord(param: SwimmingRecordRequest)
+}
+
+extension RecordService: TargetType{
+    var baseURL: URL {
+        return URL(string: GeneralAPI.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .sendSwimmingRecord:
+            return "/record"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self{
+        case .sendSwimmingRecord:
+            return .post
+        }
+    }
+    
+    var sampleData: Data {
+        return "@@".data(using: .utf8)!
+    }
+    
+    var task: Task {
+        switch self {
+        case .sendSwimmingRecord(let param):
+            return .requestJSONEncodable(param)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        default:
+            return ["Content-Type": "application/json"]
+        }
+    }
+}
