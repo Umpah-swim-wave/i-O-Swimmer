e82062813062f68bc21e0b81f8602dbab795731b
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-02-12 13:03:05 +0900

    ✅[CHORE] StrokeView 코드 수정 완료(#77) - StrokeHeaderView 추가

--- Umpah-iOS/Umpah-iOS/Screen/Main/Component/Record/StrokeView.swift
+++ Umpah-iOS/Umpah-iOS/Screen/Main/Component/Record/StrokeView.swift
@@ -7,91 +7,73 @@
 
 import UIKit
 
-import Then
 import SnapKit
+import Then
 
-class StrokeView: UIView {
-    // MARK: - Properties
-    lazy var strokeTableView = UITableView().then {
-        $0.backgroundColor = .clear
+final class StrokeView: UIView {
+    
+    // MARK: - properties
+    
+    private lazy var strokeTableView = UITableView().then {
         $0.delegate = self
         $0.dataSource = self
-        $0.contentInset = UIEdgeInsets(top: -20, left: 0, bottom: 0, right: 0)
+        $0.backgroundColor = .clear
         $0.separatorColor = .clear
         $0.separatorStyle = .none
+        $0.contentInset = UIEdgeInsets(top: -20, left: 0, bottom: 0, right: 0)
         $0.register(InsideStrokeTVC.self, forCellReuseIdentifier: InsideStrokeTVC.className)
     }
-    
-    let strokes: [String] = ["자유형", "평영", "배영", "접영", "혼영"]
+    private let strokeTypes: [String] = ["자유형", "평영", "배영", "접영", "혼영"]
 
+    // MARK: - init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        makeShadow(.upuhSkyBlue, 0.6, CGSize(width: 0, height: 0), 7)
-        setupLayout()
+        render()
+        configUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupLayout() {
+    // MARK: - func
+    
+    private func render() {
         addSubview(strokeTableView)
+        
         strokeTableView.snp.makeConstraints {
             $0.edges.equalToSuperview()
             $0.height.equalTo(310)
         }
     }
+    
+    private func configUI() {
+        makeShadow(.upuhSkyBlue, 0.6, CGSize(width: 0, height: 0), 7)
+    }
 }
 
+// MARK: - UITableViewDataSource
 extension StrokeView: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return strokeTypes.count
     }
         
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: InsideStrokeTVC.className) as? InsideStrokeTVC else { return UITableViewCell() }
-        cell.setupLabelData(stroke: strokes[indexPath.row], index: indexPath.row)
-        cell.backgroundColor = .clear
+        cell.setupStrokeLabel(to: strokeTypes[indexPath.row], with: indexPath.row)
         return cell
     }
 }
     
+// MARK: - UITableViewDelegate
 extension StrokeView: UITableViewDelegate {
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let view = UIView()
-        let strokeLabel = UILabel().then {
-            $0.text = "영법"
-            $0.font = .IBMPlexSansRegular(ofSize: 12)
-            $0.textColor = .upuhHeaderGray
-        }
-        let distanceLabel = UILabel().then {
-            $0.text = "거리"
-            $0.font = .IBMPlexSansRegular(ofSize: 12)
-            $0.textColor = .upuhHeaderGray
-        }
-        let speedLabel = UILabel().then {
-            $0.text = "평균속도"
-            $0.font = .IBMPlexSansRegular(ofSize: 12)
-            $0.textColor = .upuhHeaderGray
-        }
-        view.addSubviews([strokeLabel, distanceLabel, speedLabel])
-        strokeLabel.snp.makeConstraints {
-            $0.top.leading.equalToSuperview().inset(24)
-            $0.bottom.equalToSuperview().inset(16)
-        }
-        distanceLabel.snp.makeConstraints {
-            $0.top.equalTo(strokeLabel)
-            $0.trailing.equalToSuperview().inset(135)
-        }
-        speedLabel.snp.makeConstraints {
-            $0.top.equalTo(strokeLabel)
-            $0.trailing.equalToSuperview().inset(43)
-        }
-        return view
+        return StrokeHeaderView()
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
