f445b69bd685e08fe3196f965e87861301eb9b9f
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-02-12 13:05:45 +0900

    ✅[CHORE] TopView 코드 수정(#77)

--- Umpah-iOS/Umpah-iOS/Screen/Main/Component/Record/TopView.swift
+++ Umpah-iOS/Umpah-iOS/Screen/Main/Component/Record/TopView.swift
@@ -7,44 +7,52 @@
 
 import UIKit
 
-import Then
 import SnapKit
+import Then
 
-class TopView: UIView {
-    // MARK: - Properties
-    var nameLabel = UILabel().then {
+final class TopView: UIView {
+    
+    // MARK: - properties
+    
+    private var nameLabel = UILabel().then {
         $0.text = "어푸님,"
         $0.textColor = .upuhBlack
         $0.font = .IBMPlexSansSemiBold(ofSize: 20)
     }
-    var titleLabel = UILabel().then {
+    private var titleLabel = UILabel().then {
         $0.text = "수영하기 좋은 날이에요!"
         $0.textColor = .upuhBlack
         $0.font = .IBMPlexSansRegular(ofSize: 18)
     }
+    
+    // MARK: - init
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupLayout()
-        backgroundColor = .clear
+        render()
+        configUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupLayout() {
-        addSubviews([nameLabel,
-                     titleLabel])
+    // MARK: - func
+    
+    private func render() {
+        addSubviews([nameLabel, titleLabel])
         
         nameLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(49)
             $0.leading.equalToSuperview().inset(36)
         }
-        
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(nameLabel.snp.bottom).offset(5)
             $0.leading.equalToSuperview().inset(36)
         }
+    }
+    
+    private func configUI() {
+        backgroundColor = .clear
     }
 }
