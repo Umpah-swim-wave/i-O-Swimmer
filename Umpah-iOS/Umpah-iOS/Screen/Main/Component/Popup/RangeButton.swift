335d91a36688af66ae69c6efa754c9c64e43de90
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-02-13 14:28:03 +0900

    ✅[CHORE] RangeButton 코드 수정(#79)

--- Umpah-iOS/Umpah-iOS/Screen/Main/Component/Popup/RangeButton.swift
+++ Umpah-iOS/Umpah-iOS/Screen/Main/Component/Popup/RangeButton.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class RangeButton: UIButton {
+final class RangeButton: UIButton {
+    
+    // MARK: - properties
     
     override var isSelected: Bool {
         didSet {
@@ -15,26 +17,38 @@ class RangeButton: UIButton {
             layer.borderColor = isSelected ? UIColor.upuhBlue.withAlphaComponent(0.2).cgColor : UIColor.upuhBlue.withAlphaComponent(0.15).cgColor
         }
     }
+    private var title: String
+    
+    // MARK: - init
 
     init(title: String) {
+        self.title = title
         super.init(frame: .zero)
+        render()
         configUI()
-        constraint(widthAnchor, constant: 62)
-        constraint(heightAnchor, constant: 40)
-        setTitle(title, for: .normal)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - func
+    
+    private func render() {
+        constraint(widthAnchor, constant: 62)
+        constraint(heightAnchor, constant: 40)
+    }
+    
     private func configUI() {
+        contentEdgeInsets = UIEdgeInsets(top: 13, left: 18, bottom: 13, right: 18)
         clipsToBounds = true
+
         titleLabel?.font = .IBMPlexSansSemiBold(ofSize: 14)
+        setTitle(title, for: .normal)
         setTitleColor(.upuhGreen, for: .normal)
+
         layer.borderColor = UIColor.upuhBlue.withAlphaComponent(0.15).cgColor
         layer.borderWidth = 2
         layer.cornerRadius = 21
-        contentEdgeInsets = UIEdgeInsets(top: 13, left: 18, bottom: 13, right: 18)
     }
 }
