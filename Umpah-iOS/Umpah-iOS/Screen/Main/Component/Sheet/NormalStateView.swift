a4b860cefc09f23b5bc49765ae82d4804c22b42a
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-02-18 01:40:30 +0900

    ✅[CHORE] NormalStateView 코드 수정(#81)

--- Umpah-iOS/Umpah-iOS/Screen/Main/Component/Sheet/NormalStateView.swift
+++ Umpah-iOS/Umpah-iOS/Screen/Main/Component/Sheet/NormalStateView.swift
@@ -7,32 +7,30 @@
 
 import UIKit
 
-import Then
 import SnapKit
+import Then
 
-class NormalStateView: UIView {
-    // MARK: - Properties
-    let titleLabel = UILabel().then {
+final class NormalStateView: BaseView {
+    
+    // MARK: - properties
+    
+    private let titleLabel = UILabel().then {
         $0.font = .IBMPlexSansSemiBold(ofSize: 16)
         $0.textColor = .upuhGreen
     }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupLayout()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK: - Custom Method
-    private func setupLayout() {
+
+    override func render() {
         addSubview(titleLabel)
         
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(34)
             $0.centerX.equalToSuperview()
         }
+    }
+    
+    // MARK: - func
+    
+    func setupTitle(to title: String) {
+        titleLabel.text = title
     }
 }
